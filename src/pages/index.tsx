fb3ccee91628b9072c6065609c10e5b66916cab0
martnght12 <martnght@gmail.com>
2021-02-25 00:18:25 -0300

    adicionando title e favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { CompletedChallengers } from "../components/CompletedChallengers";
+import { Countdown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExpericenceBar";
 import { Profile } from "../components/Profile";
+import { Challenger } from "../components/Challenger";
+
+import Head from 'next/head';
 
 //styles
 import styles from '../styles/pages/Home.module.css';
@@ -8,15 +12,19 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home | Move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div className={styles.leftContainer}>
           <Profile />
           <CompletedChallengers />
+          <Countdown />
         </div>
         <div>
-          <h1>Desafios</h1>
+          <Challenger />
         </div>
       </section>
     </div>
